54ce6d8a4871347e466fd4c6f43bef0c256975a7
TM957 <nachole957@gmail.com>
2024-09-10 20:15:21 -0600

    The SECRET_KEY code is commented out because it's not necessary, and Vercel doesn't work due to database issues.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pymysql
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SECRET_KEY'] = 'tu_secret_key_super_secreta'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/todo_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
